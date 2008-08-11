78f08eaa67c0239cc7970e8a04d003b2fc9edba0
Magnus Holm <judofyr@gmail.com>
2008-08-11 19:20:27 +0200

    Added Cheater

---
+++ bots/cheater.rb
@@ -0,0 +1,24 @@
+#
+# Cheater, always wins!
+# 
+# Created by Magnus Holm <judofyr@gmail.comv>
+# 
+
+class RPS::Round
+  alias old_execute execute
+  def execute
+    if me = [first_player, second_player].detect { |player| player.is_a?(Cheater) }
+      @winner = me
+    else
+      old_execute
+    end
+  end
+end
+
+class Cheater
+  include RPS::Bot
+  
+  def next
+    moves.first
+  end
+end
